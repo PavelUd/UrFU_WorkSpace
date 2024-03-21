6b29f496d3e5554327661c9162930b5687185c9e
PavelUd <paveludin4@gmail.com>
2024-03-22 00:53:35 +0500

    init project

---
+++ UrFU_WorkSpace/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UrFU_WorkSpace.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
